40a6008975c0d6d9a5dd3c36a67541399bd19da3
Precursos Kaijuhead <143108922+Precursos@users.noreply.github.com>
2026-09-18 02:29:00 -0600

    update 18/09/2026

--- scenes/asteroids/asteroid.gd
+++ scenes/asteroids/asteroid.gd
@@ -1,8 +1,21 @@
 extends Area2D
 
-@export var speed: float
+@export var min_speed: float
+@export var max_speed: float
+@export var min_rotation_speed: float
+@export var max_rotation_speed: float
 
+var random_speed
+var random_rotation_speed
+
+func _ready():
+	random_speed = randf_range(min_speed, max_speed)
+	random_rotation_speed = randf_range(min_rotation_speed, max_rotation_speed)
+	
 
 func _process(delta):
-	position.x -= speed * delta #Se utiliza delta para que corra igual en todas las PC sin que se agregun más FPS.
+	position.x -= random_speed * delta #Se utiliza delta para que corra igual en todas las PC sin que se agregun más FPS.
+	rotation_degrees += random_rotation_speed * delta #aplicando la rotación
+	
+	
 	
--- scenes/main/asteroid_spawner.gd
+++ scenes/main/asteroid_spawner.gd
@@ -2,9 +2,14 @@ extends Marker2D
 
 @export var big_asteroid_scene: PackedScene # <-- Es el lugar donde se guarda la escena
 
+@export var min_y: float
+@export var max_y: float
+
 func create_asteroid():
 	var big_asteroid_instance = big_asteroid_scene.instantiate()
 	add_child(big_asteroid_instance)
+	var random_y = randf_range(min_y, max_y)
+	big_asteroid_instance.global_position.y = random_y
 
 
 func _on_timer_timeout():
